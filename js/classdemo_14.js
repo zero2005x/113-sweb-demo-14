034415e68f9478a99411b7194bbdaf438a95fed1
zero2005x <zero2005x@gmail.com>
2025-01-02 21:23:02 +0800

    Chrome 顯 (Vercel)，顯 final_p3_xx.html 下的 P3_Projects_xx

--- js/classdemo_14.js
+++ js/classdemo_14.js
@@ -1,7 +1,7 @@
 // Select the container element where demos will be displayed
-const p = document.querySelector(".show-classdemo");
-const q = document.querySelector(".tictactoe");
-console.log("show-classdemo", p);
+const p = document.querySelector('.show-classdemo')
+const q = document.querySelector('.tictactoe')
+console.log('show-classdemo', p)
 
 /**
  * Displays different demo pages within an iframe based on the selected item
@@ -9,31 +9,35 @@ console.log("show-classdemo", p);
  */
 function showClassDemo(item) {
   switch (item) {
-    case "w02_intro_14":
+    case 'w02_intro_14':
       // Load the introduction demo page
-      p.innerHTML = `<iframe src='./demo/w02-intro-14/index.html' width="100%" height="100%"></iframe>`;
-      break;
-    case "w03_card_14":
+      p.innerHTML = `<iframe src='./demo/w02-intro-14/index.html' width="100%" height="100%"></iframe>`
+      break
+    case 'w03_card_14':
       // Load the card demo page
-      p.innerHTML = `<iframe src='./demo/w03-card-14/index.html' width="100%" height="100%"></iframe>`;
-      break;
-    case "w06_blog_14":
+      p.innerHTML = `<iframe src='./demo/w03-card-14/index.html' width="100%" height="100%"></iframe>`
+      break
+    case 'w06_blog_14':
       // Load the blog demo page
-      p.innerHTML = `<iframe src='./demo/w06-blog-14/index.html' width="100%" height="100%"></iframe>`;
-      break;
+      p.innerHTML = `<iframe src='./demo/w06-blog-14/index.html' width="100%" height="100%"></iframe>`
+      break
 
-    case "w07_nav_14":
+    case 'w07_nav_14':
       // Load the blog demo page
-      p.innerHTML = `<iframe src='./demo/w07_showdemo_14/w07_showdemo_14.html' width="100%" height="100%"></iframe>`;
-      break;
+      p.innerHTML = `<iframe src='./demo/w07_showdemo_14/w07_showdemo_14.html' width="100%" height="100%"></iframe>`
+      break
 
-    case "tictoctoe":
+    case 'tictoctoe':
       // Load the blog demo page
-      p.innerHTML = `<iframe src='./tictoctoe_14.html' width="100%" height="100%"></iframe>`;
-      break;
+      p.innerHTML = `<iframe src='./tictoctoe_14.html' width="100%" height="100%"></iframe>`
+      break
+    case 'Final_14':
+      // Load the blog demo page
+      p.innerHTML = `<iframe src='./exams/final1_14/final_p1_14.html' width="100%" height="100%"></iframe>`
+      break
 
     default:
       // Handle any unmatched cases (no action needed)
-      break;
+      break
   }
 }
